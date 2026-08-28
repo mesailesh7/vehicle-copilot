67786549e947d2aada5e6bdfc8f5fda75f9bb084
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-08-27 23:54:37 -0600

    Docker Files

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlmodel import SQLModel, create_engine, Session
 
-sqlite_file_name = "vehicle_copilot.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+sqlite_file_name = os.getenv("DATABASE_FILE", "vehicle_copilot.db")
+if sqlite_file_name.startswith("sqlite://"):
+    sqlite_url = sqlite_file_name
+else:
+    sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True, connect_args={"check_same_thread": False})
 
